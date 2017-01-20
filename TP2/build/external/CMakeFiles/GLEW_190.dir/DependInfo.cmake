355bc45bfc20e62606fb50721c105f73705b50f6
Talenie <alicia.abonnenc@hotmail.com>
2017-01-20 08:06:07 +0100

    debut du TP2

---
+++ TP2/build/external/CMakeFiles/GLEW_190.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/alicia/Cours/S8/SI/TP3D/TP2/external/glew-1.9.0/src/glew.c" "/home/alicia/Cours/S8/SI/TP3D/TP2/build/external/CMakeFiles/GLEW_190.dir/glew-1.9.0/src/glew.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  "_GLFW_HAS_DLOPEN"
+  "_GLFW_HAS_XRANDR"
+  "_UNIX"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/glfw-2.7.6/lib/x11"
+  "../external/glfw-2.7.6/lib"
+  "../external/glfw-2.7.6/include"
+  "../external/glew-1.9.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
