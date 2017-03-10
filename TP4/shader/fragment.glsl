9436d354fc7df2de05208a230bfbdd4e070c2307
Talenie <alicia.abonnenc@hotmail.com>
2017-03-10 08:54:09 +0100

    DEbut de Toon shading

--- TP4/shader/fragment.glsl
+++ TP4/shader/fragment.glsl
@@ -1,9 +1,17 @@
 // Version d'OpenGL
 #version 330
 
+/* pour le shading de Phong non flat
+*/
+in vec3 ambiant;
+in vec3 diffus;
+in vec3 speculaire;
+
+/* Flat shading 
 flat in vec3 ambiant;
 flat in vec3 diffus;
 flat in vec3 speculaire;
+*/
 
 out vec4 frag_color;
 
@@ -12,5 +20,17 @@ void main() {
 
   // Affectation de la couleur du fragment
   vec3 my_color = ambiant + diffus + speculaire;
-  frag_color = vec4(my_color,1.0);;
+  
+  // Toon shading
+  float choix = 5;
+  float x,y,z;
+  
+  for(float i = 1; i <= choix; i++){
+	if(my_color.x < i/choix && my_color.x >= (i-1)/choix) x = (i-1)/choix;
+	if(my_color.y < i/choix && my_color.y >= (i-1)/choix) y = (i-1)/choix;
+	if(my_color.z < i/choix && my_color.z >= (i-1)/choix) z = (i-1)/choix;
+  }
+  
+  frag_color = vec4(x,y,z,1.0);
+  
 }
