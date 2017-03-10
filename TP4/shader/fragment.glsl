2f4ebb115100a0d061edd179629c669cba1d2198
Talenie <alicia.abonnenc@hotmail.com>
2017-03-10 09:36:07 +0100

    Shading de fong dnas le fragment shader (le vrai cette fois

--- TP4/shader/fragment.glsl
+++ TP4/shader/fragment.glsl
@@ -3,20 +3,53 @@
 
 /* pour le shading de Phong non flat
 */
-in vec3 ambiant;
-in vec3 diffus;
-in vec3 speculaire;
+in vec3 in_normale;
+in vec3 lumiere;
 
 /* Flat shading 
-flat in vec3 ambiant;
-flat in vec3 diffus;
-flat in vec3 speculaire;
+flat in vec3 normale;
+flat in vec3 lumiere;
 */
+
+// Parametres
+uniform mat4 ModelMatrix;
+uniform mat4 ViewMatrix;
+uniform mat4 ProjectionMatrix;
+
 
 out vec4 frag_color;
 
 // Fonction appellee pour chaque fragment
 void main() {
+  vec3 normale = normalize(in_normale);
+  float ra,rd,rs;
+  ra = 0.3;
+  rd = 0.9;
+  rs = 1.0;
+  
+  vec3 la,ld,ls;
+    
+  la = vec3(1.0,0.6,0.2);
+  ld = vec3(1.0,1.0,0.1);
+  ls = vec3(1.0,1.0,1.0);
+  
+  
+  int s = 16;
+  
+  vec3 e = normalize((inverse(ViewMatrix * ModelMatrix) * vec4(0,0,1.0,0)).xyz);
+  
+  vec3 l,r;
+   
+  // Lumière venant de la caméra
+  l = normalize(-e);
+  //l = lumiere;
+  r = normalize(reflect(l, normale));
+  
+  vec3 ambiant = ra*la;
+  vec3 diffus = rd*ld*max(-dot(normale,l),0);
+  vec3 speculaire = rs*ls*pow(max(dot(e,r),0),s);
+
+
 
   // Affectation de la couleur du fragment
   vec3 my_color = ambiant + diffus + speculaire;
--- TP4/shader/vertex.glsl
+++ TP4/shader/vertex.glsl
@@ -9,14 +9,12 @@ in vec3 in_normal;
 // Donnees de sortie
 /* pour le shading de phong non flat
 */
-out vec3 ambiant;
-out vec3 diffus;
-out vec3 speculaire;
+out vec3 in_normale;
+out vec3 lumiere;
 
 /* Flat shading
-flat out vec3 ambiant;
-flat out vec3 diffus;
-flat out vec3 speculaire;
+flat out vec3 normale;
+flat out vec3 lumiere;
  */
  
 // Parametres
@@ -30,34 +28,6 @@ void main() {
   // gl_Position est definit par defaut dan GLSL
   gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(in_position, 1.0);
   
-  vec3 normale = (transpose(inverse(ModelMatrix)) * vec4(in_normal,0.0)).xyz;
-  
-  float ra,rd,rs;
-  ra = 0.3;
-  rd = 0.9;
-  rs = 1.0;
-  
-  vec3 la,ld,ls;
-  
-  la = in_color;
-  ld = in_color;
-  
-  //la = vec3(1.0,0.6,0.2);
-  //ld = vec3(1.0,1.0,0.1);
-  ls = vec3(1.0,1.0,1.0);
-  
-  
-  int s = 16;
-  
-  vec3 e = normalize((inverse(ViewMatrix * ModelMatrix) * vec4(0,0,1.0,0)).xyz);
-  
-  vec3 l,r;
-   
-  // Lumière venant de la caméra
-  l = normalize(-e);
-  r = normalize(reflect(l, normale));
-  
-  ambiant = ra*la;
-  diffus = rd*ld*max(-dot(normale,l),0);
-  speculaire = rs*ls*pow(max(dot(e,r),0),s);
+  in_normale = (transpose(inverse(ModelMatrix)) * vec4(in_normal,0.0)).xyz;
+  lumiere = vec3(0.0,1.0,0.5);
 }
