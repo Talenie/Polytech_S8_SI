3c66b97bac4495e8121884c59117306419a606d3
Gilles B <bonhoure.gilles@gmail.com>
2017-03-10 08:55:10 +0100

    j'ai fait des trucs random

--- TP4/shader/fragment.glsl
+++ TP4/shader/fragment.glsl
@@ -12,5 +12,13 @@ void main() {
 
   // Affectation de la couleur du fragment
   vec3 my_color = ambiant + diffus + speculaire;
-  frag_color = vec4(my_color,1.0);;
+  
+  if(diffus.x	 > 0.04) {
+  	my_color = vec3(1.0,1.0,1.0);
+  } else {
+  	my_color = vec3(0.0,0.0,0.0);
+  }
+  
+  frag_color = vec4(my_color,1.0);
+  
 }
